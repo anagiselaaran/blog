7cc5d7b8594241266f44fe5db758eb8e0611291d
Ana Aran <anita.gisela.aran@gmail.com>
2024-06-30 12:17:38 +0200

    estilos en formulario, creacion html footer con estilos

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,12 @@
 import { Routes } from '@angular/router';
 import { ListaPostsComponent } from './pages/lista-posts/lista-posts.component';
 import { FormularioComponent } from './pages/formulario/formulario.component';
+import { CopyrightComponent } from './pages/copyright/copyright.component';
 
 export const routes: Routes = [
     { path: '' , pathMatch:'full', redirectTo:'/posts'},
     { path: 'posts', component: ListaPostsComponent },
     { path: 'new', component: FormularioComponent },
+    { path:'copyright', component:CopyrightComponent},
     { path: '**', component:ListaPostsComponent}
 ];
---
+++ src/app/pages/copyright/copyright.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'copyright',
+  standalone: true,
+  imports: [],
+  templateUrl: './copyright.component.html',
+  styleUrl: './copyright.component.css'
+})
+export class CopyrightComponent {
+
+}
